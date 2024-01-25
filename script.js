4c571acc7d0890d4483c4fbbe99f4ec7c30f6fe8
Destiny3016 <157696033+Destiny3016@users.noreply.github.com>
2024-01-25 13:22:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yay, see you on the 29th!";
+  gif.src =
+    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  // this will hide the "No" button after you click "Yes"
+  noBtn.style.display = "none";
+ // this will center the "Yes" button when the "No" button is hidden
+  yesBtn.style.margin = "0 auto";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
